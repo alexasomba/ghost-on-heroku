8044e498f95b3eba879146c1877f2cea6eb39f62
Alexander Asomba <alexanderasomba@gmail.com>
2019-03-25 07:39:53 +0100

    Update serverjs
    Added Fixie Sock for static IP to connect to remote MySQL

--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 var ghost = require('ghost');
 var cluster = require('cluster');
 
+// Alex added FIXIE SOCKS
+'use strict';
+
+const SocksConnection = require('socksjs');
+const mysql = require('mysql2');
+const fixieUrl = process.env.FIXIE_SOCKS_HOST;
+const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'));
+
+const mysqlServer = {
+  host: 'your-host.example.com',
+  port: 3306
+};
+
+const dbUser = 'automk7b_asomba';
+const dbPassword = 'Uja97EbIQJsJn8s9gD';
+const db = 'automk7b_asomba';
+
+const fixieConnection = new SocksConnection(mysqlServer, {
+  user: fixieValues[0],
+  pass: fixieValues[1],
+  host: fixieValues[2],
+  port: fixieValues[3],
+});
+
+const mysqlConnPool = mysql.createPool({
+  user: dbUser,
+  password: dbPassword,
+  database: db,
+  stream: fixieConnection
+});
+
+mysqlConnPool.getConnection(function gotConnection(err, connection) {
+
+  if (err) throw err;
+
+  queryVersion(connection);
+});
+
+function queryVersion(connection) {
+  connection.query('SELECT version();', function (err, rows, fields) {
+
+      if (err) throw err;
+
+      console.log('MySQL/MariaDB version: ', rows);
+      connection.release();
+      process.exit();
+  });
+}
+
 // Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
 var WORKERS = process.env.WEB_CONCURRENCY || 1;
 
